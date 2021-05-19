2082dc002dafb84329bcbd3b210b4b27fccad624
EvgenStr <juve2910@gmail.com>
2021-05-19 11:20:50 +0300

    init repository

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 "use strict";
 const comp = require("./Component");
 const lodash = require("lodash");
+require("./styles.css");
 const test = 5;
 console.log(test);
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,40 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   mode: "development",
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Output Management",
+      title: "Hello Webpack",
+      template: "./index.html",
     }),
   ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+  },
   output: {
-    filename: "main.js",
+    filename: "[contenthash].js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
       },
     ],
   },
